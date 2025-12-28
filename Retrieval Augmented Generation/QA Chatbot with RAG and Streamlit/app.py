c9f1d2b421ec2a99b5ba819d572953b48f0a83e1
André Amaral <103210621+oAndreAmaral@users.noreply.github.com>
2025-12-28 17:27:27 +0000

    RAG with chatbot on streamlit

---
+++ Retrieval Augmented Generation/QA Chatbot with RAG and Streamlit/app.py
@@ -0,0 +1,40 @@
+import os
+import streamlit as st
+
+# get the LLM RAG functions
+from rag_utility import ingestion_pdf_to_vectordatabase, retrieval_vectordatabase_to_response
+
+# set the directory
+working_directory = os.path.dirname(os.path.abspath((__file__)))
+
+# STREAMLIT: set the title for the streamlit app
+st.title("Smart Article Reviewer")
+
+# STREAMLIT: upload multiple pdf files
+uploaded_files = st.file_uploader("Upload a PDF Article file", type=["pdf"], accept_multiple_files=True)
+
+if uploaded_files:
+    for uploaded_file in uploaded_files:
+        if uploaded_file is not None:
+
+            save_path = os.path.join(working_directory, uploaded_file.name) # generate the path where to save the file
+            with open(save_path, "wb") as f:
+                f.write(uploaded_file.getbuffer()) # save the file in the right directory
+
+            # LLM RAG: ingestion process
+            process_document = ingestion_pdf_to_vectordatabase(uploaded_file.name)
+
+    st.info("Document Processed Successefully") # inform the user about the upload in the chatbot
+
+# STREAMLIT: get the user input
+user_question = st.text_area("What is your question about the document?")
+
+if st.button("Answer"):
+
+    # LLM RAG: retrieval process
+    answer, source_name = retrieval_vectordatabase_to_response(user_question)
+
+    # STREAMLIT: print the response and source
+    st.markdown("Agent Response:")
+    st.markdown(answer)
+    st.markdown(source_name)
---
+++ Retrieval Augmented Generation/QA Chatbot with RAG and Streamlit/rag_utility.py
@@ -0,0 +1,74 @@
+import os
+from dotenv import load_dotenv
+
+from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_groq import ChatGroq
+from langchain_classic.chains import RetrievalQA
+
+load_dotenv() # read the environmental variables
+
+working_directory = os.path.dirname(os.path.abspath((__file__))) # Get the working directory where is this python script
+
+embedding = HuggingFaceEmbeddings() # use the embedding default
+
+llm = ChatGroq(
+    model="llama-3.1-8b-instant",
+    temperature=0.0
+)
+
+def ingestion_pdf_to_vectordatabase(file_name):
+
+    # Loading the directory in the loader with the specific folders and load the documents
+    loader = PyPDFLoader(f"{working_directory}/{file_name}")
+
+    documents = loader.load() # store the files in the variable list documents (length = number of documents ||  documents[x] - gives the document x content)
+
+    # Split the documents into smaller chunks
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 2000,
+        chunk_overlap = 200
+    )
+
+    text_chunks = text_splitter.split_documents(documents)
+
+    # Store the text chunks in the database
+    vector_store = Chroma(
+        persist_directory = f"{working_directory}/doc_vector_db",
+        embedding_function=embedding
+    )
+
+    vector_store.add_documents(text_chunks) # with add_documents we can append the embeddings from new documents instead of replace the old one.
+
+    return 0
+
+def retrieval_vectordatabase_to_response(user_question):
+
+    # Load the vector data from the database
+    vector_store = Chroma(
+        embedding_function=embedding,
+        persist_directory=f"{working_directory}/doc_vector_db"
+    )
+
+    # Create a retriever
+    retriever = vector_store.as_retriever()
+
+    # Prepare the Question and Awnser chain
+    qa_chain = RetrievalQA.from_chain_type(
+        llm = llm,
+        chain_type = "stuff",
+        retriever = retriever,
+        return_source_documents = True
+    )
+
+    # get the response from the llm
+    response = qa_chain.invoke({"query":user_question})
+    answer = response['result']
+
+    for source in response["source_documents"]:
+        file_path = source.metadata["source"]  # pega o caminho completo
+        source_name = os.path.basename(file_path)  # extrai só o nome do arquivo
+
+    return answer, source_name
